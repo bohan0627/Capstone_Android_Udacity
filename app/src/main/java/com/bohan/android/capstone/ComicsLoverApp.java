bdc4726577033f02acaab0697fb0e376c85decd6
BoHan <han.bo1@husky.neu.edu>
2018-11-11 16:26:49 -0500

    Add LeakCanary library

--- app/src/main/java/com/bohan/android/capstone/ComicsLoverApp.java
+++ app/src/main/java/com/bohan/android/capstone/ComicsLoverApp.java
@@ -1,9 +1,46 @@
 package com.bohan.android.capstone;
 
 import android.app.Application;
+import android.content.Context;
 
 /**
  * Created by Bo Han.
  */
 public class ComicsLoverApp extends Application {
+    private static ComicsLoverAppComponent comicserAppComponent;
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+
+        if (comicserAppComponent == null) {
+            comicserAppComponent = DaggerComicsLoverAppComponent.builder()
+                    .comicserAppModule(new ComicsLoverAppModule(this))
+                    .build();
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // We should not init our app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+        // Normal app init code below.
+
+        if (BuildConfig.DEBUG) {
+            Timber.uprootAll();
+            Timber.plant(new Timber.DebugTree());
+
+            Stetho.initializeWithDefaults(this);
+        }
+    }
+
+    public static ComicserAppComponent getAppComponent() {
+        return comicserAppComponent;
+    }
 }
